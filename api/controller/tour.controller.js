194700da38d3a606c2fd95806b105bb4ae88008d
Sabah <sabahalmel7@gmail.com>
2023-10-24 16:59:20 +0300

    finished hopefully

--- api/controller/tour.controller.js
+++ api/controller/tour.controller.js
@@ -1,12 +1,30 @@
-import {
-  getAllTours,
-  addNewTour,
-  findId,
-  updateTour,
-  deleteTour,
-} from "../models/tour.model.js";
+import toursDb from "../models/tour.models.js";
+
+let defaultId = 0;
+
+async function getLatestId() {
+  const lastId = await toursDb.findOne().sort("-id");
+  if (!lastId) {
+    return defaultId;
+  }
+  return lastId.id;
+}
+async function saveTour(tour) {
+  try {
+    await toursDb.updateOne(
+      { title: tour.title },
+      { ...tour },
+      { upsert: true }
+    );
+  } catch (err) {
+    console.error(`could not save tour ${err}`);
+  }
+}
 async function httpGetAllTours(req, res) {
-  return res.status(200).json(await getAllTours());
+  let getAllTours = await toursDb
+    .find({}, { _id: 0, __v: 0 })
+    .sort({ title: 1 });
+  return res.status(200).json(await getAllTours);
 }
 
 async function httpAddNewTour(req, res) {
@@ -18,7 +36,9 @@ async function httpAddNewTour(req, res) {
     return res.status(400).json({ error: "missing image" });
   } else {
     tour.image = image;
-    await addNewTour(tour);
+    const newid = (await getLatestId()) + 1;
+    const newTour = Object.assign(tour, { id: newid });
+    await saveTour(newTour);
     return res.status(200).json(tour);
   }
 }
@@ -26,19 +46,25 @@ async function httpAddNewTour(req, res) {
 async function httpUpdateTour(req, res) {
   const tour = req.body;
   const image = req.file.path;
+  const found = await toursDb.findOne({ id: tour.id });
   if (!tour.id) {
     return res.status(400).json({ error: "enter an id" });
   }
-  if (!(await findId(tour.id))) {
+  if (!found) {
     return res.status(400).json({ error: "id not found" });
   }
   tour.image = image;
-  await updateTour(tour);
+  try {
+    await toursDb.updateOne({ id: tour.id }, { ...tour });
+  } catch (err) {
+    console.error(`could not save tour ${err}`);
+  }
   return res.status(200).json(tour);
 }
 
 async function httpDeleteTour(req, res) {
-  if (await deleteTour(req.params.id)) {
+  const found = await toursDb.findOneAndDelete({ id: req.params.id });
+  if (found) {
     return res.status(200).json("deleted");
   } else {
     return res.status(404).json("Tours not found");
--- api/utils/multer.js
+++ api/utils/multer.js
@@ -10,7 +10,7 @@ import path from "path";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "images");
+    cb(null, "imagesTour");
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
