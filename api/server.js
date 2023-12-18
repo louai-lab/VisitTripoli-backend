fcfbe228aad1b1182341812388ce02bf8a65fd47
Abdulrahman Ghassa <abdulrahman.ghassa93@gmail.com>
2023-12-18 10:14:46 +0200

    initialize sequelize

--- api/server.js
+++ api/server.js
@@ -1,46 +1,34 @@
 import express from "express";
 import cors from "cors";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import 'dotenv/config'
+import sequelize from "./config/dbConnection.js"
+
 import userRouter from './routes/user.routes.js'
-
-import hotelRouter from "./routes/hotel-route.js";
-
-dotenv.config();
-
 import { toursRouter } from "./routes/tour.router.js";
 import locationRouter from "./routes/locations.routes.js";
+import hotelRouter from "./routes/hotel-route.js";
 
-// express app
 const app = express();
 app.use(express.json());
 
 app.use(cors());
 
+try {
+  await sequelize.authenticate()
+  console.log('connection established!')
+}
+catch (error) {
+  console.log('unable to connect!')
+}
+
 app.use("/tours", toursRouter);
 app.use("/locations", locationRouter);
 app.use("/api/hotel", hotelRouter);
+app.use("/api/hotel", hotelRouter);
+app.use('/user',userRouter)
 
-app.use("/images", express.static("images"));
-async function startServer() {
-  mongoose.connection.once("open", () => {
-    console.log("mongo is ready");
-  });
-
-  mongoose.connection.on("error", (err) => {
-    console.error(err);
-  });
-  await mongoose.connect(process.env.MONG_ULI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+app.use("/images", express.static("images"))
 
   app.listen(process.env.PORT, () => {
     console.log("listening on port: " + process.env.PORT);
   });
-}
-
-startServer();
-
-app.use("/api/hotel", hotelRouter);
-app.use('/user',userRouter)
