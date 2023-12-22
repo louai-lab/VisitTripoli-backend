831f4d01395a9ff87b8b658fc14d48a21c8d738b
Abdulrahman Ghassa <abdulrahman.ghassa93@gmail.com>
2023-12-22 16:28:34 +0200

    Location and TourLocation tables created

---
+++ api/models/location.js
@@ -0,0 +1,18 @@
+import { DataTypes, Sequelize } from "sequelize"
+import sequelize from "../config/dbConnection.js"
+
+const location = sequelize.define(
+    'location',
+    {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }
+)
+
+export default location
---
+++ api/models/tourLocation.js
@@ -0,0 +1,18 @@
+import { DataTypes, Sequelize } from "sequelize"
+import sequelize from "../config/dbConnection.js"
+
+const tourLocation = sequelize.define(
+    'location',
+    {
+        tourId: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        locationId: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    }
+)
+
+export default tourLocation
